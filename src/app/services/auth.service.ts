c4683793de1a0e22b2129aa77dd3cc46c4a7c7d3
carlosrs14 <crincones578@gmail.com>
2025-03-30 17:02:44 -0500

    allowing to use jwt in front end

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { environment } from '../../enviroments/enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = environment.apiUrl +'login/'
+  constructor(private http: HttpClient) { }
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(this.apiUrl, { username, password });
+  }
+}
